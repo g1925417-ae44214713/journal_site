e0e9ebc99cc1a4dd485d93a3f4b25502bc61b165
g1925417 <g1925417@yahoo.co.jp>
2020-03-10 02:22:25 +0900

    add データの投稿

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -8,4 +8,20 @@ class ArticlesController < ApplicationController
     @articles = Article.all
   end
 
+  def new
+    @article = Article.new
+  end
+
+  def create
+    @article = Article.new(article_params)
+    @article.save
+    redirect_to @article
+  end
+
+  private
+
+  def article_params
+  params.require(:article).permit(:title, :body)
+  end
+  
 end
