9d3e7221d80fb5de172bf15560ec992e00184442
g1925417 <g1925417@yahoo.co.jp>
2020-03-11 10:23:12 +0900

    エラー表示の削除

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -14,11 +14,8 @@ class ArticlesController < ApplicationController
 
   def create
     @article = Article.new(article_params)
-    if @article.save
-      redirect_to @article
-    else
-      render :new
-    end
+    @article.save
+    redirect_to articles_path
   end
 
   def edit
